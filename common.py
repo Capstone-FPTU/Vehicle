ae7def5afb0c4256c06a66ca0a82053a834bf7af
LinhND <linhnguyen131999@gmail.com>
2021-10-25 21:53:31 +0700

    update common

---
+++ common.py
@@ -0,0 +1,14 @@
+
+# Login MQTT
+CODE = "VH001"
+USERNAME = "vehicle_1"
+PASSWORD = "Aa123456789"
+HOST = "96cd623ad07c45fb886782e9f77079d2.s1.eu.hivemq.cloud"
+PORT = 8883
+
+# URL call API
+# API_ENDPOINT = "http://localhost:8080"
+API_ENDPOINT = "https://sc-mavr-api.herokuapp.com"
+
+URI_SEND_NOTI = "/api/vehicle/login"
+URI_START_DELIVERY = "/api/vehicle/start-delivery-order"
--- start.py
+++ start.py
@@ -1,9 +1,51 @@
-from paho.mqtt import client
-from subscribe import *
+from paho.mqtt import client as mqtt
+from common import *
+from main import run
+import json
+import requests
+import getmac
 import time
 
-# start_client = threading.Thread(target=start)
-# start_client.start()
+
+def connect() -> mqtt:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            mac_address = getmac.get_mac_address()
+            print(mac_address)
+            print(USERNAME+" Connected successfully")
+            x = requests.get(API_ENDPOINT + URI_SEND_NOTI+"?vehicle_code=" + CODE + "&mac_address="+mac_address)
+            print(x.status_code)
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+    client.username_pw_set(USERNAME, PASSWORD)
+    client.connect(HOST, PORT)
+    return client
+
+def on_message(client, userdata, msg):
+    print("Received message: " + msg.topic + " -> " + msg.payload.decode("utf-8"))
+    messages = msg.payload.decode("utf-8")
+    if((msg.topic == "sc-mavr/vehicle/check-connect") and (messages == "MASTER")):
+        client.publish("sc-mavr/server/check-connect",CODE)
+    turn_value = ''
+    if(msg.topic == "sc-mavr/vehicle/order"):
+        data = json.loads(messages)
+        if(data["code"] == CODE):
+            for key, value in data["theWay"].items():
+                if key == "HOME":
+                    turn_value = value.upper()
+            print(turn_value)
+            run(data["theWay"], turn_value)
+def start():
+    client = connect()
+    client.subscribe("sc-mavr/vehicle/check-connect")
+    client.subscribe("sc-mavr/vehicle/order")
+    client.on_message = on_message
+    client.loop_forever()
+
+# START
 start()
 time.sleep(0.5)
 
